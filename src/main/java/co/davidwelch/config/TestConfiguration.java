d1cd6ecc2b5e0b153b4e67f11e5abb794f0d5bc4
David Welch <david@davidwelch.co>
2012-05-06 16:11:06 -0600

    Cleaned up examples a little

--- src/main/java/co/davidwelch/config/TestConfiguration.java
+++ src/main/java/co/davidwelch/config/TestConfiguration.java
@@ -1,5 +1,12 @@
 package co.davidwelch.config;
 
+import java.net.InetSocketAddress;
+import java.util.Scanner;
+import java.util.concurrent.Executors;
+
+import org.jboss.netty.bootstrap.Bootstrap;
+import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.DefaultResourceLoader;
@@ -8,6 +15,7 @@ import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
 
 import co.davidwelch.cdi.AnnotationProcessor;
+import co.davidwelch.test.netty.HttpServerPipelineFactory;
 import co.davidwelch.test.netty.SomeBean;
 
 @Configuration
@@ -25,5 +33,25 @@ public class TestConfiguration {
 	@Bean
 	public SomeBean getSomeBean(){
 		return new SomeBean();
+	}
+	
+	@Bean
+	public ServerBootstrap getBootstrap(){
+		System.out.println("Starting");
+		// Configure the server.
+		ServerBootstrap bootstrap = new ServerBootstrap(
+				new NioServerSocketChannelFactory(
+						Executors.newCachedThreadPool(),
+						Executors.newCachedThreadPool()));
+
+		System.out.println("Setting pipeline Factory");
+		// Set up the event pipeline factory.
+		bootstrap.setPipelineFactory(new HttpServerPipelineFactory());
+
+//		System.out.println("Binding");
+//		// Bind and start to accept incoming connections.
+//		bootstrap.bind(new InetSocketAddress(8080));
+		
+		return bootstrap;
 	}
 }
---
+++ src/main/java/co/davidwelch/test/netty/HttpServerPipelineFactory.java
@@ -0,0 +1,39 @@
+package co.davidwelch.test.netty;
+
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.DefaultChannelPipeline;
+import org.jboss.netty.handler.codec.http.HttpChunkAggregator;
+import org.jboss.netty.handler.codec.http.HttpContentCompressor;
+import org.jboss.netty.handler.codec.http.HttpRequestDecoder;
+import org.jboss.netty.handler.codec.http.HttpResponseEncoder;
+
+public class HttpServerPipelineFactory implements ChannelPipelineFactory {
+	
+	public static final Integer MAX_UPLOAD_SIZE = Integer.MAX_VALUE;
+	
+	public ChannelPipeline getPipeline() throws Exception {
+		// Create a default pipeline implementation.
+		ChannelPipeline pipeline = new DefaultChannelPipeline();
+
+		// Uncomment the following line if you want HTTPS
+		// SSLEngine engine = null;
+		// //SecureChatSslContextFactory.getServerContext().createSSLEngine();
+		// engine.setUseClientMode(false);
+		// pipeline.addLast("ssl", new SslHandler(engine));
+
+		pipeline.addLast("decoder", new HttpRequestDecoder());
+		// Uncomment the following line if you don't want to handle
+		// HttpChunks.
+
+		pipeline.addLast("aggregator", new HttpChunkAggregator(MAX_UPLOAD_SIZE));
+		pipeline.addLast("encoder", new HttpResponseEncoder());
+
+		// Remove the following line if you don't want automatic content
+		// compression.
+		pipeline.addLast("deflater", new HttpContentCompressor());
+		pipeline.addLast("handler", new HttpRequestHandler());
+
+		return pipeline;
+	}
+}
